df6b326fcb493b23e81d9e980a9f7c9cb1324f61
Moh Hidayah <kmohhidayah@gmail.com>
2023-01-21 01:15:03 +0800

    update

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -80,8 +80,14 @@ return packer.startup(function(use)
 	use("williamboman/nvim-lsp-installer") -- simple to use language server installer
 	use("tamago324/nlsp-settings.nvim") -- language server settings defined in json for
 	--[[ use("jose-elias-alvarez/null-ls.nvim") -- for formatters and linters ]]
-  use({ "glepnir/lspsaga.nvim", branch = "main" })
-	-- Telescope
+  use({
+    "glepnir/lspsaga.nvim",
+    branch = "main",
+    config = function()
+        require('lspsaga').setup({})
+    end,
+  })
+  -- Telescope
 	use("nvim-telescope/telescope.nvim")
 
 	-- Treesitter
